7ab3a790918d62ddd884dcd910d3936e2d7cfc9f
MatthewVicente <41347019+MatthewVicente@users.noreply.github.com>
2019-05-20 21:40:26 -0300

    Add listaLigada class to project

--- src/simuladorgerenciamentodememoria/SimuladorGerenciamentoDeMemória.java
+++ src/simuladorgerenciamentodememoria/SimuladorGerenciamentoDeMemória.java
@@ -14,7 +14,14 @@ public class SimuladorGerenciamentoDeMemória {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        listaLigada lista = new listaLigada();
         
+        lista.addOrdenado(10);
+        lista.addOrdenado(20);
+        lista.addOrdenado(15);
+        lista.addOrdenado(1);
+        lista.addOrdenado(30);
+        
+        System.out.println(lista);
     }
-    
 }
---
+++ src/simuladorgerenciamentodememoria/listaLigada.java
@@ -0,0 +1,89 @@
+package simuladorgerenciamentodememoria;
+
+public class listaLigada {
+    private No inicio;// endereço inicial da lista
+
+    public listaLigada(){
+        this.inicio = null; // lista esta vazia
+    }
+    public void addInicio( int elemento ){
+        this.inicio = new No(elemento,this.inicio);
+    }
+    
+    public int remInicio(){
+        //Antes de remover devemos verificar se temos pelo menos um Nó na lista,
+        // não faz sentido remover algo que não existe, 
+        if( !this.isEmpty()){ // se a lista nao esta vazia
+            No ant = this.inicio; // guarda a celula que serah removida
+            // avançar a referência que aponta para o primeiro  Nó (inicio) 
+            // para o próximo Nó da lista
+            this.inicio = this.inicio.getProx();
+            return ant.getElemento();
+        }
+        else
+            throw new RuntimeException("lista vazia!");
+
+    }
+    public boolean isEmpty(){
+        return this.inicio == null;
+    } 
+
+    @Override
+    public String toString() {
+        return this.inicio+"";
+    }   
+    // versao iterativa
+    public boolean busca(int x) {
+        
+        for(No aux=this.inicio; aux!=null;aux=aux.getProx())
+            if( aux.getElemento()==x)
+                return true;
+        
+        return false;
+    }
+    // versao recursiva
+    public boolean buscaRec(int x ){
+        return busca(x, this.inicio);
+    }
+    private boolean busca(int x, No aux){
+        // condicoes de parada
+        if( aux == null )
+            return false;
+        
+        if( aux.getElemento()==x)
+            return true;
+        
+         return busca(x, aux.getProx());
+    }
+
+    public void addOrdenado(int x) {
+        No aux = this.inicio;
+        No ant=null;
+        while( aux !=null&&aux.getElemento()<x){
+            ant = aux;
+            aux = aux.getProx();
+        }
+        No novo = new No(x, aux);
+        if( ant == null )// insere  no inicio
+            this.inicio = novo;
+        else
+            ant.setProx(novo);
+        
+    }
+    public void addFim(int x) {
+        No aux = this.inicio;
+        No ant=null;
+        while( aux !=null){
+            ant = aux;
+            aux = aux.getProx();
+        }
+        No novo = new No(x, null);
+        if( ant == null )// insere  no inicio
+            this.inicio = novo;
+        else
+            ant.setProx(novo);
+        
+    }
+    
+}
+
